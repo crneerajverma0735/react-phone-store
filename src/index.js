e313e30c4c186b3f8a5d5aace8d5662bbcdd88bb
Neeraj verma <verma.neeraj735@gmail.com>
2020-06-19 15:01:52 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import { ProductProvider } from './Context';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ProductProvider>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </ProductProvider>,
   document.getElementById('root')
 );
 
